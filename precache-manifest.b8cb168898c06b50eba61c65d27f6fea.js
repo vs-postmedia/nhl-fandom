e9132bc1f18f22badfb14ca9f845966ddf04a7b5
Nathan Griffiths <ngriffiths@postmedia.com>
2019-09-26 12:49:33 -0700

    Updates

---
+++ precache-manifest.b8cb168898c06b50eba61c65d27f6fea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e2769928fdde897a1251c58381ed16",
+    "url": "/nhl-fandom/index.html"
+  },
+  {
+    "revision": "fdc6425258789962cb92",
+    "url": "/nhl-fandom/static/css/main.51507c8a.chunk.css"
+  },
+  {
+    "revision": "19d50e7613a989a19b3f",
+    "url": "/nhl-fandom/static/js/2.2ce84108.chunk.js"
+  },
+  {
+    "revision": "fdc6425258789962cb92",
+    "url": "/nhl-fandom/static/js/main.728fce50.chunk.js"
+  },
+  {
+    "revision": "e192abbb3e52b7ad0211",
+    "url": "/nhl-fandom/static/js/runtime~main.6d99d702.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/nhl-fandom/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/nhl-fandom/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/nhl-fandom/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/nhl-fandom/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/nhl-fandom/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/nhl-fandom/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
